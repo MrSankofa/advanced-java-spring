f4721dfed1f9c24dcb9a166d410628a28e604203
brett.cunningham <brett.cunningham@sbevision.com>
2024-12-11 17:21:58 -0500

    fix(config): making these changes enable the application to build because it is only focusing on MyDemoApplication

--- src/main/java/com/codingnomads/springsecurity/authentication/usernamepassword/security/SecurityConfig.java
+++ src/main/java/com/codingnomads/springsecurity/authentication/usernamepassword/security/SecurityConfig.java
@@ -1,10 +1,6 @@
 /* CodingNomads (C)2024 */
 package com.codingnomads.springsecurity.authentication.usernamepassword.security;
 
-import com.codingnomads.springsecurity.authentication.usernamepassword.models.Authority;
-import com.codingnomads.springsecurity.authentication.usernamepassword.models.RoleEnum;
-import com.codingnomads.springsecurity.authentication.usernamepassword.models.UserPrincipal;
-import java.util.Collections;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,8 +21,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
                 // start the process of customizing authorization settings.
                 // this will be covered in depth in the authorization chapter.
                 .authorizeHttpRequests(auth -> auth
@@ -54,24 +46,25 @@ public class SecurityConfig {
     // the following beans are commented out to avoid conflict with CustomUserDetailsService
     // comment out the @Service annotation inside CustomUserDetailsService before uncommenting either of these
 
-//    @Bean
-//    public UserDetailsManager jdbcUserDetails(DataSource dataSource) {
-//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-//        users.setAuthoritiesByUsernameQuery("SELECT a.id, a.authority FROM authorities a \n"
-//                + "JOIN user_authority_join_table uajt ON a.id = uajt.authority_id \n"
-//                + "JOIN users u ON u.id = uajt.user_id \n"
-//                + "WHERE u.username = ?");
-//        users.setUsersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?");
-//        return users;
-//    }
-//
-//    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//        Authority userAuth = Authority.builder().authority(RoleEnum.ROLE_USER).build();
-//        UserPrincipal user1 =
-//                new UserPrincipal("USER1", passwordEncoder().encode("hi"), Collections.singletonList(userAuth));
-//        UserPrincipal user2 =
-//                new UserPrincipal("USER2", passwordEncoder().encode("hello"), Collections.singletonList(userAuth));
-//        return new InMemoryUserDetailsManager(user1, user2);
-//    }
+    //    @Bean
+    //    public UserDetailsManager jdbcUserDetails(DataSource dataSource) {
+    //        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    //        users.setAuthoritiesByUsernameQuery("SELECT a.id, a.authority FROM authorities a \n"
+    //                + "JOIN user_authority_join_table uajt ON a.id = uajt.authority_id \n"
+    //                + "JOIN users u ON u.id = uajt.user_id \n"
+    //                + "WHERE u.username = ?");
+    //        users.setUsersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?");
+    //        return users;
+    //    }
+    //
+    //    @Bean
+    //    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+    //        Authority userAuth = Authority.builder().authority(RoleEnum.ROLE_USER).build();
+    //        UserPrincipal user1 =
+    //                new UserPrincipal("USER1", passwordEncoder().encode("hi"), Collections.singletonList(userAuth));
+    //        UserPrincipal user2 =
+    //                new UserPrincipal("USER2", passwordEncoder().encode("hello"),
+    // Collections.singletonList(userAuth));
+    //        return new InMemoryUserDetailsManager(user1, user2);
+    //    }
 }
