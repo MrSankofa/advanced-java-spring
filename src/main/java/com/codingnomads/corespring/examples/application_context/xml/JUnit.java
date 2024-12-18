7286f91147dfda80d50cf205038b426dbe5d66a3
brett.cunningham <brett.cunningham@sbevision.com>
2024-12-18 16:33:06 -0500

    completed(exercise): used xml instead of the annotations to populate the pojo with data using the component scan

---
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/JUnit.java
@@ -0,0 +1,13 @@
+package com.codingnomads.corespring.examples.application_context.xml;
+
+public class JUnit {
+  private String version;
+
+  public String getVersion() {
+    return version;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
+}
