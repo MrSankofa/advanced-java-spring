344cfe8df218cda066dbc9edab4dbb41d9226df7
brett.cunningham <brett.cunningham@sbevision.com>
2024-12-18 19:31:49 -0500

    completed(exercise): to create custom annotation

---
+++ src/main/java/com/codingnomads/corespring/examples/springbeanlifecycle/NewBeanNameAware.java
@@ -0,0 +1,13 @@
+package com.codingnomads.corespring.examples.springbeanlifecycle;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewBeanNameAware implements BeanNameAware {
+
+  @Override
+  public void setBeanName(String name) {
+    System.out.println("Something quirky to the console :) ");
+  }
+}
